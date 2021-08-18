a176b6a5dd801f6ff5909e2709e8c7f6dc2242cb
Nick Nicholas <opoudjis@gmail.com>
2021-08-18 13:52:29 +1000

    security update: https://github.com/nsip/mrlf-syllabus-viewer/issues/4

--- cmd/staticserver/server.go
+++ cmd/staticserver/server.go
@@ -3,9 +3,9 @@ package main
 import (
 	// "net/http"
 
-	"github.com/labstack/echo"
+	"github.com/labstack/echo/v4"
 	// "github.com/labstack/echo/engine/standard"
-	"github.com/labstack/echo/middleware"
+	"github.com/labstack/echo/v4/middleware"
 
 	"log"
 )
